c2274b83067aa30d951b1d9ca3fb735cc520af68
jnsprngs <jnsprngs@gmail.com>
2018-04-25 12:59:44 -0400

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,9 @@
+/* setup namespaces */
+//prebid
+var pbjs = pbjs || {};
+pbjs.que = pbjs.que || [];
+//dfp
+var googletag = googletag || {};
+googletag.cmd = googletag.cmd || [];
+//publife
+var publife = publife || {};
